e02c3c3ab223801eb5b4cdb5b451979223335358
unifoPraveen <praveen.sb@unifo.in>
2023-11-16 20:48:31 +0530

    added listAssets route

--- backend/src/routes/listAssets.js
+++ backend/src/routes/listAssets.js
@@ -1,27 +1,34 @@
-import { dbGet } from "../cloudFunctions/index.js"
+import { dbQuery } from "../cloudFunctions/index.js"
 import { WrappedConsole } from "../utils/wrappedConsole.js"
 
 export const listAssets = async (req, res) => {
   const wrappedConsole = new WrappedConsole("GET")
-  let assetList
-  const query = {
-    "TableName": "assetBeePlus",
-    "KeyConditionExpression": "#kn0 = :kv0",
-    "IndexName": "type-index",
-    "ExpressionAttributeNames": {
-      "#kn0": "type"
-    },
-    "ExpressionAttributeValues": {
-      ":kv0": "Monitor"
-    },
-    "Select": "ALL_PROJECTED_ATTRIBUTES"
+  let userQuery = JSON.parse(JSON.stringify(req.query))
+  let reqKey = Object.keys(userQuery)
+  if(reqKey[0] === "type" || reqKey[0] === "userId"){
+    let assetList
+    const query = {
+      "TableName": "assetBeePlus",
+      "KeyConditionExpression": "#kn0 = :kv0",
+      "IndexName": reqKey[0]+"-index",
+      "ExpressionAttributeNames": {
+        "#kn0": reqKey[0]
+      },
+      "ExpressionAttributeValues": {
+        ":kv0": userQuery[reqKey[0]]
+      },
+      "Select": "ALL_PROJECTED_ATTRIBUTES"
+    }
+    try {
+      assetList = await dbQuery(query)
+      res.send(assetList.Items)
+    } catch (error) {
+      wrappedConsole.error("Error while fetching asset list ", error)
+      res.send(false)
+    }
   }
-  try {
-    assetList = await dbGet(query)
-  } catch (error) {
-    wrappedConsole.error("Error while fetching asset list ", error)
+  else{
     res.send(false)
   }
 
-  res.send(assetList)
 }
